9c3f14478ac5eb81e17f5f9aa0ba2fa990634c50
ccik <ccik@24d2b540-2039-5a44-a0d7-d7bc2ff27355>
2013-04-28 11:57:19 +0000

    Added initial implementation of class NtHandle.
    git-svn-id: https://fallen:8443/svn/ntnix/ntnix/trunk@16 24d2b540-2039-5a44-a0d7-d7bc2ff27355

---
+++ NtWrappers/NtHandle.h
@@ -0,0 +1,35 @@
+#include <ntndk.h>
+
+class NtHandle
+{
+public:
+    NtHandle()
+        : m_Handle(NULL)
+    {}
+
+    NtHandle(HANDLE Handle)
+        : m_Handle(Handle)
+    {}
+
+    ~NtHandle()
+    {
+        if (m_Handle)
+            NtClose(m_Handle);
+    }
+
+    PHANDLE operator&()
+    {
+        if (m_Handle)
+            NtClose(m_Handle);
+
+        return &m_Handle;
+    }
+
+    operator HANDLE()
+    {
+        return m_Handle;
+    }
+
+private:
+    HANDLE m_Handle;
+};
