c8900f158bfadd82b0c18fc3ce140de25784402c
ccik <ccik@24d2b540-2039-5a44-a0d7-d7bc2ff27355>
2013-04-30 22:31:35 +0000

    Added class SafeNtLpcHandle -- a wrapper for LPC port handles.
    git-svn-id: https://fallen:8443/svn/ntnix/ntnix/trunk@54 24d2b540-2039-5a44-a0d7-d7bc2ff27355

---
+++ src/NtLibrary/Microsoft.WinNT.SafeHandles/SafeNtLpcHandle.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.WinNT.SafeHandles
+{
+    public sealed class SafeNtLpcHandle : SafeNtHandle
+    {
+        internal SafeNtLpcHandle(System.IntPtr preexistingHandle)
+            : base(preexistingHandle) { }
+
+        internal SafeNtLpcHandle(System.IntPtr preexistingHandle, bool ownsHandle)
+            : base(preexistingHandle, ownsHandle) { }
+    }
+}
