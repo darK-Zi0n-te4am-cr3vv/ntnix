7af153fcc2766dee858838bbb5af45e12da910db
ccik <ccik@24d2b540-2039-5a44-a0d7-d7bc2ff27355>
2013-04-30 20:47:35 +0000

    Expanded macro API_ENTRY_STUB -- defining an dllexport emtpy routine.
    git-svn-id: https://fallen:8443/svn/ntnix/ntnix/trunk@53 24d2b540-2039-5a44-a0d7-d7bc2ff27355

--- src/psxdll/stubs.h
+++ src/psxdll/stubs.h
@@ -1,6 +1,10 @@
 #ifndef _STUBS_H_
 #define _STUBS_H_
 
-#define API_ENTRY_STUB(Name) /* Empty */
+#define API_ENTRY_STUB(Name)                                                  \
+    __declspec(dllexport) void Name ()                                        \
+    {                                                                         \
+        /* Empty */                                                           \
+    }                                                                         \
 
 #endif /* _STUBS_H_ */
