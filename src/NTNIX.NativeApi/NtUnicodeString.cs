32d24d0d2524f0636faa25eee6a49f86654355e6
ccik <ccik@24d2b540-2039-5a44-a0d7-d7bc2ff27355>
2013-05-01 13:24:15 +0000

    Added class NtUnicodeString to NTINIX.NativePai.
    git-svn-id: https://fallen:8443/svn/ntnix/ntnix/trunk@57 24d2b540-2039-5a44-a0d7-d7bc2ff27355

---
+++ src/NTNIX.NativeApi/NtUnicodeString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NTNIX.NativeApi
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct NtUnicodeString : IDisposable
+    {
+        [MarshalAs(UnmanagedType.U2)]
+        private ushort Length;
+        [MarshalAs(UnmanagedType.U2)]
+        private ushort MaximumLength;
+        [MarshalAs(UnmanagedType.LPWStr)]
+        private IntPtr Buffer;
+
+        public NtUnicodeString(string str)
+        {
+            Length = (ushort)(str.Length * 2);
+            MaximumLength = (ushort)(Length + 2);
+            Buffer = Marshal.StringToHGlobalUni(str);
+        }
+
+        public override string ToString()
+        {
+            return Marshal.PtrToStringUni(Buffer);
+        }
+
+        public void Dispose()
+        {
+            Marshal.FreeHGlobal(Buffer);
+            Buffer = IntPtr.Zero;
+        }
+    }
+}
