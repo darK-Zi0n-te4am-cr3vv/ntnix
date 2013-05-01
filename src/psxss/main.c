df5b2767c4aa8c81b4b4ceef606c6d2a30c9b483
ccik <ccik@24d2b540-2039-5a44-a0d7-d7bc2ff27355>
2013-05-01 15:38:49 +0000

    Added main.c to psxss. The _X86_ preprocessor directive is used for windef.h.
    git-svn-id: https://fallen:8443/svn/ntnix/ntnix/trunk@64 24d2b540-2039-5a44-a0d7-d7bc2ff27355

---
+++ src/psxss/main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <windef.h>
+
+int main(int argc, wchar_t **argv)
+{
+    UNREFERENCED_PARAMETER(argc);
+    UNREFERENCED_PARAMETER(argv);
+
+    return EXIT_SUCCESS;
+}
